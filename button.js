9e91f694a38dff846475bef46ff8b5d749b26b63
MadosMark <69796358+MadosMark@users.noreply.github.com>
2020-12-11 13:23:53 +0100

    Update

---
+++ button.js
@@ -0,0 +1,8 @@
+
+(function(document){
+    const button = document.querySelector("button");
+    button.addEventListener('click', function() {
+        location.href = "nonono.html";
+});
+
+})(document);
