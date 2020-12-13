d9bc4d817a469c94183375eb444f1d5a9e1160cb
MadosMark <69796358+MadosMark@users.noreply.github.com>
2020-12-13 19:07:49 +0100

    Update

--- js/renderGifs.js
+++ js/renderGifs.js
@@ -7,6 +7,14 @@ const gifs = [
     {id: '6', url: 'https://media.giphy.com/media/ToMjGpx9F5ktZw8qPUQ/source.gif'},
     {id: '7', url: 'https://media.giphy.com/media/ToMjGpx9F5ktZw8qPUQ/source.gif'},
     {id: '8', url: 'https://media.giphy.com/media/ToMjGpx9F5ktZw8qPUQ/source.gif'},
+    {id: '9', url: 'https://media.giphy.com/media/ToMjGpx9F5ktZw8qPUQ/source.gif'},
+    {id: '10', url: 'https://media.giphy.com/media/ToMjGpx9F5ktZw8qPUQ/source.gif'},
+    {id: '11', url: 'https://media.giphy.com/media/ToMjGpx9F5ktZw8qPUQ/source.gif'},
+    {id: '12', url: 'https://media.giphy.com/media/ToMjGpx9F5ktZw8qPUQ/source.gif'},
+    {id: '13', url: 'https://media.giphy.com/media/ToMjGpx9F5ktZw8qPUQ/source.gif'},
+    {id: '14', url: 'https://media.giphy.com/media/ToMjGpx9F5ktZw8qPUQ/source.gif'},
+    {id: '15', url: 'https://media.giphy.com/media/ToMjGpx9F5ktZw8qPUQ/source.gif'},
+    {id: '16', url: 'https://media.giphy.com/media/ToMjGpx9F5ktZw8qPUQ/source.gif'},
   ]
 
 const gifId = document.getElementById('gifId')
